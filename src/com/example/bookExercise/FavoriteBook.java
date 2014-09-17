83f0bbe6808d0211736277d9dd32832343877aaf
elyselturner <elyse.turner@detroitlabs.com>
2014-09-17 09:02:52 -0400

    I added a book object git status

---
+++ src/com/example/bookExercise/FavoriteBook.java
@@ -0,0 +1,8 @@
+package com.example.bookExercise;
+
+/**
+ * Created by elyseturner on 9/17/14.
+ */
+public class FavoriteBook {
+    
+}
---
+++ src/com/example/bookExercise/bookExerciseDemo.java
@@ -0,0 +1,8 @@
+package com.example.bookExercise;
+
+/**
+ * Created by elyseturner on 9/16/14.
+ */
+public class bookExerciseDemo {
+
+}
